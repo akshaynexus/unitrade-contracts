96d6eb3d591c5bfb981cd4685ca6b861bd8d7ffd
Marcelo Morgado <cmarcelom@gmail.com>
2020-12-22 16:47:23 +0000

    Updating migration script

--- deploy/index.ts
+++ deploy/index.ts
@@ -5,6 +5,7 @@ import UniTradeIncinerator from "../build/UniTradeIncinerator.json"
 import UniTradeOrderBook from "../build/UniTradeOrderBook.json"
 import IUniswapV2Router from "../build/IUniswapV2Router02.json"
 import IUniswapV2Factory from "../build/IUniswapV2Factory.json"
+import UniTradeMarketOrders from "../build/UniTradeMarketOrders.json"
 
 require("dotenv").config()
 
@@ -18,41 +19,31 @@ const logContractData = (environment: string, name: string, contract: Contract) 
   console.log("")
 }
 
-const deployWithSigner = (environment: string, signer: Signer, uniswapRouterAddress: string, unitradeTokenAddress: string) => {
-  let unitradeStaker: Contract,
-      unitradeIncinerator: Contract
-
-  deployContract(signer, UniTradeStaker01, [unitradeTokenAddress])
-    .then((_unitradeStaker: Contract) => {
-      unitradeStaker = _unitradeStaker
-      logContractData(environment, "UniTradeStaker01", _unitradeStaker)
-    })
-    .then(() => {
-      return deployContract(signer, UniTradeIncinerator, [uniswapRouterAddress, unitradeTokenAddress])
-    })
-    .then((_unitradeIncinerator: Contract) => {
-      unitradeIncinerator = _unitradeIncinerator
-      logContractData(environment, "UniTradeIncinerator", _unitradeIncinerator)
-    })
-    .then(() => {
-      return deployContract(signer, UniTradeOrderBook, [
-        uniswapRouterAddress, 
-        unitradeIncinerator.address, 
-        unitradeStaker.address, 
-        2, // feeMul
-        1000, // feeDiv
-        6, // splitMul
-        10 // splitDiv
-      ])
-    })
-    .then((_unitradeOrderBook: Contract) => {
-      logContractData(environment, "UniTradeOrderBook", _unitradeOrderBook)
-    })
-    .catch(console.error)
+const deployWithSigner = async (environment: string, signer: Signer, uniswapRouterAddress: string, unitradeTokenAddress: string) => {
+  try {
+    const unitradeStaker: Contract = await deployContract(signer, UniTradeStaker01, [unitradeTokenAddress])
+    logContractData(environment, "UniTradeStaker01", unitradeStaker)
+    const unitradeIncinerator: Contract = await deployContract(signer, UniTradeIncinerator, [uniswapRouterAddress, unitradeTokenAddress])
+    logContractData(environment, "UniTradeIncinerator", unitradeIncinerator)
+    const unitradeOrderBook: Contract = await deployContract(signer, UniTradeOrderBook, [
+      uniswapRouterAddress,
+      unitradeIncinerator.address,
+      unitradeStaker.address,
+      2, // feeMul
+      1000, // feeDiv
+      6, // splitMul
+      10, // splitDiv
+    ])
+    logContractData(environment, "UniTradeOrderBook", unitradeOrderBook)
+    const uniTradeMarketOrders = await deployContract(signer, UniTradeMarketOrders, [unitradeOrderBook.address])
+    logContractData(environment, "UniTradeMarketOrders", uniTradeMarketOrders)
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 const ticker = (environment: string, timeout: number, signer: Signer, uniswapRouterAddress: string) => {
-  const unitradeTokenAddress: string = (process.env.UNITRADE_CONTRACT_ADDRESS || "")
+  const unitradeTokenAddress: string = process.env.UNITRADE_CONTRACT_ADDRESS || ""
   if (!unitradeTokenAddress) throw new Error("set UNITRADE_CONTRACT_ADDRESS")
 
   console.log("")
@@ -64,9 +55,9 @@ const ticker = (environment: string, timeout: number, signer: Signer, uniswapRou
   console.log(`continuing in ${timeout} seconds`)
   console.log("")
 
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const tick = setInterval(async () => {
-      process.stdout.write(`${timeout}... `);
+      process.stdout.write(`${timeout}... `)
 
       timeout--
       if (timeout >= 0) return
@@ -85,7 +76,7 @@ const publicDeploy = async (envName: string, envPrivateKey: string | undefined, 
   if (!envPrivateKey) throw new Error(`Set ${envName.toUpperCase()}_DEPLOYMENT_PRIVATE_KEY`)
   const provider: providers.BaseProvider = providers.getDefaultProvider(envName)
   const signer: Signer = new Wallet(envPrivateKey, provider)
-  const uniswapRouterAddress: string = (process.env.UNISWAP_V2_ROUTER || "")
+  const uniswapRouterAddress: string = process.env.UNISWAP_V2_ROUTER || ""
   if (!uniswapRouterAddress) throw new Error("set UNISWAP_V2_ROUTER")
   await ticker(envName, timeout, signer, uniswapRouterAddress)
 }
@@ -93,9 +84,9 @@ const publicDeploy = async (envName: string, envPrivateKey: string | undefined, 
 const makeUniswapMocks = async (signer: Signer) => {
   const mockUniswapV2Factory: Contract = await deployMockContract(signer, IUniswapV2Factory.abi)
   const mockUniswapV2Router: Contract = await deployMockContract(signer, IUniswapV2Router.abi)
-  
-  await mockUniswapV2Router.mock.factory.returns(mockUniswapV2Factory.address);
-  
+
+  await mockUniswapV2Router.mock.factory.returns(mockUniswapV2Factory.address)
+
   return mockUniswapV2Router.address
 }
 
@@ -154,4 +145,3 @@ try {
 } catch (error) {
   console.error(error)
 }
-
